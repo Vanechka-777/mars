ed7ddb98be5f0e1c4abae9ef75628a08118719cb
Vanechka-777 <vanya.chugunov2009@gmail.com>
2026-03-02 19:08:22 +0300

    Готовимся к миссии

---
+++ flask/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/d')
+def glav():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
