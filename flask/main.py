b757e03c718fccbfcb0a30a3564acee06713a952
Vanechka-777 <vanya.chugunov2009@gmail.com>
2026-03-05 16:48:02 +0300

    Список профессий

--- flask/main.py
+++ flask/main.py
@@ -22,5 +22,14 @@ def training(prof):
         return render_template('prof_scientist.html', prof=prof)
 
 
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    if list_type == 'ol':
+        return render_template('ol.html')
+    elif list_type == 'ul':
+        return render_template('ul.html')
+
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
