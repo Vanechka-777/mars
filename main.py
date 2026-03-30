655eb2302ca226fb4eff1d414d4869d8dc9d5870
vanya <vanya.chugunov2009@gmail.com>
2026-03-30 16:02:15 +0300

    поменяли структуру

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, make_response
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
