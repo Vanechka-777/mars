eb0de7502a91f7cf17bdceaa52c1bce57da33048
Vanechka-777 <vanya.chugunov2009@gmail.com>
2026-04-01 18:47:19 +0300

    3 задача

--- add_users.py
+++ add_users.py
@@ -3,15 +3,45 @@ from data import db_session
 
 db_session.global_init("db/mars.db")
 
-user = User()
-user.name = "Ivan"
-user.surname = "Chugunov"
-user.email = "vanya@email.ru"
-user.age = "17"
-user.position = "modul 1"
-user.speciality = "sis. admin"
-user.address = "Vurnary"
-user.password = "fffffff"
-db_sess = db_session.create_session()
-db_sess.add(user)
-db_sess.commit()
+captain = User()
+captain.surname = "Scott"
+captain.name = "Ridley"
+captain.age = 21
+captain.position = "captain"
+captain.speciality = "research engineer"
+captain.address = "module_1"
+captain.email = "scott_chief@mars.org"
+captain.password = "captain123"
+
+colonists = [
+    {
+        'surname': 'Ivanov',
+        'name': 'Petr',
+        'age': 28,
+        'position': 'engineer',
+        'speciality': 'mechanic',
+        'address': 'module_2',
+        'email': 'ivanov@mars.org',
+        'password': 'ivanov123'
+    },
+    {
+        'surname': 'Kuznetsova',
+        'name': 'Elena',
+        'age': 25,
+        'position': 'doctor',
+        'speciality': 'therapist',
+        'address': 'module_3',
+        'email': 'kuznetsova@mars.org',
+        'password': 'kuznetsova123'
+    },
+    {
+        'surname': 'Volkov',
+        'name': 'Alexey',
+        'age': 32,
+        'position': 'scientist',
+        'speciality': 'astrobiologist',
+        'address': 'module_1',
+        'email': 'volkov@mars.org',
+        'password': 'volkov123'
+    }
+]
