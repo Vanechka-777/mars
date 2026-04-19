a2d3cd2e2dae4172384ebdf2beb00dd6bf414e22
Vanechka-777 <vanya.chugunov2009@gmail.com>
2026-04-19 17:00:49 +0300

    фласк 2 задание 4

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-from flask import Flask, request, make_response
-from flask_restful import reqparse, abort, Api, Resource
-from data import jobs_api
-from data import users_resources, db_session
+from flask import Flask, render_template
 
 app = Flask(__name__)
-api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("db/mars.db")
 
 
-@app.route("/cookie_test")
-def cookie_test():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    if visits_count:
-        res = make_response(
-            f"Вы пришли на эту страницу {visits_count + 1} раз")
-        res.set_cookie("visits_count", str(visits_count + 1),
-                       max_age=60 * 60 * 24 * 365 * 2)
-    else:
-        res = make_response(
-            "Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',
-                       max_age=60 * 60 * 24 * 365 * 2)
-    return res
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    context = {
+        'title': 'Анкета кандидата',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'выше среднего',
+        'profession': 'пилот марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True'
+    }
+    return render_template('auto_answer.html', **context)
 
 if __name__ == '__main__':
-    app.register_blueprint(jobs_api.blueprint)
-    api.add_resource(users_resources.UsersListResource, '/api/v2/users')
-    api.add_resource(users_resources.UserResource, '/api/v2/users/<int:user_id>')
     app.run(port=5000, host='127.0.0.1')
