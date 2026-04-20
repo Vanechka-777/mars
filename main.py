2c28dbc63eb716f40241e53a821199d1e719c639
vanya <vanya.chugunov2009@gmail.com>
2026-04-20 16:39:34 +0300

    фласк 2 задание 4

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+@app.route('/')
+def glav():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+        return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    return render_template('list_prof.html', list_type=list_type)
 
 @app.route('/answer')
 @app.route('/auto_answer')
